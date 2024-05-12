6b09325306059fd21ba4bb09c4c51da97ab3f780
hikaricp <dockerc@163.com>
2024-05-13 01:36:06 +0800

    添加 python pack

--- lua/community.lua
+++ lua/community.lua
@@ -9,6 +9,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.code-runner.executor-nvim" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.gruvbox-baby" },
